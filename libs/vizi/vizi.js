a98ab5e3a0ae32818d588c4cc58f8ded9386c9c5
Tony Parisi <tparisi@gmail.com>
2013-10-29 12:13:00 -0700

    Now with fixed Vizi.js zoom

--- libs/vizi/vizi.js
+++ libs/vizi/vizi.js
@@ -48964,7 +48964,7 @@ Vizi.OrbitControls = function ( object, domElement ) {
 
 	function getZoomScale() {
 
-		return Math.pow( 0.95, scope.userZoomSpeed );
+		return Math.pow( 1 / 0.95, scope.userZoomSpeed );
 
 	}
 
@@ -49086,10 +49086,10 @@ Vizi.OrbitControls = function ( object, domElement ) {
 			if (touch0 && touch1) {
 				 var touchDistance = calcDistance(event.touches[0], event.touches[1]);
 				 var deltaDistance = touchDistance - scope.touchDistance;
-				 if (deltaDistance < 0) {
+				 if (deltaDistance > 0) {
 					 scope.zoomIn();
 				 }
-				 else if (deltaDistance > 0) {
+				 else if (deltaDistance < 0) {
 					 scope.zoomOut();
 				 }
 				 scope.touchDistance = touchDistance;
@@ -49184,11 +49184,11 @@ Vizi.OrbitControls = function ( object, domElement ) {
 
 		if ( delta > 0 ) {
 
-			scope.zoomOut();
+			scope.zoomIn();
 
 		} else {
 
-			scope.zoomIn();
+			scope.zoomOut();
 
 		}
 
